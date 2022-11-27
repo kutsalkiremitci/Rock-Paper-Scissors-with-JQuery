f053aee351c36f8a208c118da6ca811f7c253fb7
kutsalkiremitci <kutsalkiremitci@gmail.com>
2022-11-27 16:47:42 +0300

    create-project

---
+++ public/js/main.js
@@ -0,0 +1,63 @@
+/*
+    Rock Paper Scissors with JQuery
+    https://www.github.com/kutsalkiremitci
+    27.11.2022
+*/
+
+let choice;
+let choices = ["rock","paper","scissors"];
+let computerChoice;
+let elections = {
+    "rock": {
+        "rock": "draw",
+        "paper": "win",
+        "scissors": "lose"
+    },
+    "paper": {
+        "paper": "draw",
+        "rock": "win",
+        "scissors": "lose"
+    },
+    "scissors":{
+        "scissors": "draw",
+        "rock": "win",
+        "paper": "lose"
+    },
+};
+let messageStyles = {
+    "draw": "warning",
+    "win": "success",
+    "lose": "danger"
+};
+/* Wrappers */
+let choiceWrapper = $('.choice-wrapper');
+let endGameWrapper = $('.endGame-wrapper');
+let computerchoiceArea = $('.computerchoice');
+let myChoiceArea = $('.myChoice');
+let resultArea = $('.resultArea');
+
+/* Select Your Choice */
+$('div[data-choice]').on('click',function(){
+    /* Hide & Show Wrappers */
+    choiceWrapper.addClass('d-none');
+    endGameWrapper.removeClass('d-none');
+
+    /* Your Choice */
+    choice = $(this).attr('data-choice');
+    /* Computer Choice */
+    computerChoice = choices[Math.floor(Math.random() * (choices.length))];
+
+    /* Results */
+    myChoiceArea.text(choice);
+    computerchoiceArea.text(computerChoice);
+    
+    /* Message Style Type Set & Show */
+    let result = elections[computerChoice][choice];
+    resultArea.addClass('text-' + messageStyles[result]);
+    resultArea.text(result);
+})
+/* Play Again Btn */
+$('.playAgain').on('click',function(){
+    endGameWrapper.addClass('d-none');
+    choiceWrapper.removeClass('d-none');
+})
